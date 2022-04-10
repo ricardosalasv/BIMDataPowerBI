5036380a3eb3c77928eb6b2f6f6cea8842c4e1c1
ricardosalasv <ricardo.salas.cg@gmail.com>
2022-04-09 21:02:54 -0300

    feat: PowerBI dashboard implemented

--- BIMDataPowerBI/Command4.cs
+++ BIMDataPowerBI/Command4.cs
@@ -6,7 +6,7 @@ namespace BIMDataPowerBI
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     public class Command4 : IExternalCommand
     {
-        private string url = "https://api.powerbi.com...";  // replace url value with your POWER BI Push URL
+        private string url = "https://api.powerbi.com/beta/0684ed1b-ca0f-48a7-975e-2334ad41db56/datasets/8c5c0def-956c-4e67-ae6f-29b83c38e07e/rows?noSignUpCheck=1&cmpid=pbi-glob-head-snn-signin&key=TQpepnGKpfEw8FXP1UZbFtdFEIquzEETsCPXjUfD9guKtvAG2Kgd5X1x%2B6bLIyzhpWVkc84t2jwIyG0baGBqaA%3D%3D";  // replace url value with your POWER BI Push URL
         public DataParser dataParser = new DataParser();
         public API api = new API();
         public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
